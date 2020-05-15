d7407a92466c4b6b72073ca2ab90f42a7dcfd8e8
Federico Brigante <opensource@bfred.it>
2020-05-15 19:23:54 +0200

    Update dev dependencies

--- index.test-d.ts
+++ index.test-d.ts
@@ -3,4 +3,5 @@
 import {expectType} from 'tsd';
 import {applyToLink} from '.';
 
+// eslint-disable-next-line no-undef
 expectType<void>(applyToLink(document.createElement('a'), 'https://bettersite.com'));
